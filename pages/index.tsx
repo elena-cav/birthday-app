8ef7da983d6bf8ab4b2752d2b0a80e6a261ee3de
chrisgrounds <iamchrisgrounds@gmail.com>
2022-09-01 08:52:56 +0100

    animate title colour

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,30 @@ const LandingPageTitle = styled.h1`
   margin: 0;
   line-height: 1.15;
   font-size: 4rem;
+
+  background-image: linear-gradient(
+    -225deg,
+    #231557 0%,
+    #44107a 29%,
+    #ff1361 67%,
+    #fff800 100%
+  );
+  background-size: auto auto;
+  background-clip: border-box;
+  background-size: 200% auto;
+  color: #fff;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+
+  @keyframes textclip {
+    to {
+      background-position: 200% center;
+    }
+  }
 `;
 
 export default function Home() {
